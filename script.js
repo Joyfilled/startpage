5331bc5704d248560701230107c8ed242b660f1d
Joyfilled <36140412+Joyfilled@users.noreply.github.com>
2019-06-04 23:49:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+const NAME = "joy";
+const WELCOME_MESSAGE_TEMPLATE = ["night", "morning", "afternoon", "evening"];
+
+// All shortcuts are in a `SHORTCUT_STARTER+shortcutKey` format.
+// So, for example, pressing `tab+q` would redirect you to https://google.com/?q=q
+const SHORTCUT_STARTER = 'tab'
+
+// How much time (in milliseconds) you have to press shortcutKey after pressing SHORTCUT_STARTER.
+// Also change --SHORTCUT_TIMEOUT in styles.css if you change this option.
+const SHORTCUT_TIMEOUT = 1500;
+
+// The groups of links are generated from this object. Edit it to edit the page's contents.
+// shortcutKey must hold an all-lowercase single button. Theoretically should work with values like `esc` and `f1`,
+// but intended to be used with just regular latin letters.
+const MASTER_MAP = [
+    {
+        "groupName": "media",
+        "items":[
+            {"name": "youtube", "shortcutKey": "q", "url": "https://youtube.com"},
+            {"name": "hulu", "shortcutKey": "w", "url": "https://hulu.com"},
+            {"name": "twitch", "shortcutKey": "e", "url": "https://twitch.tv"}
+        ]
+    },
+    {
+        "groupName": "social",
+        "items":[
+            {"name": "twitter", "shortcutKey": "a", "url": "https://twitter.com"},
+            {"name": "reddit", "shortcutKey": "s", "url": "https://reddit.com"},
+
+        ]
+    },
+    {
+        "groupName": "misc",
+        "items":[
+            {"name": "imgur", "shortcutKey": "z", "url": "https://imgur.com"},
+            {"name": "scammer.info", "shortcutKey": "x", "url": "scammer.info"},
+            {"name": "r6tracker", "shortcutKey": "c", "url": "https://r6.tracker.network/"}
+        ]
+    }
+]
+
+let $container = document.getElementById("content");
+let getUrl = {};
+
+let $shortcutDisplayList = document.getElementsByClassName("shortcut");
+let listeningForShortcut = false;
+let listenerTimeout;
+
+function setupWelcomeMessage(){
+    let curHours = new Date().getHours();
+    curHours = Math.floor(curHours/6); // Simply dividing current hours by 6 proves to be a good enough aproximation.
+    if (curHours == 4) curHours = 3;
+    let welcome = "good " + WELCOME_MESSAGE_TEMPLATE[curHours] + ", " + NAME;
+    document.getElementById("welcome-string").innerHTML = welcome;
+}
+
+function setupGroups(){
+    for (let i = 0; i < MASTER_MAP.length; i++){
+        let curGroupData = MASTER_MAP[i];
+
+        let group = document.createElement("div");
+        group.className = "group";
+        $container.appendChild(group);
+
+        let header = document.createElement("h1");
+        header.innerHTML = curGroupData.groupName;
+        group.appendChild(header);
+
+        for (let j = 0; j < curGroupData.items.length; j++){
+            let curItemData = curGroupData.items[j];
+
+            let pContainer = document.createElement("p");
+            group.appendChild(pContainer);
+
+            let link = document.createElement("a");
+            link.innerHTML = curItemData.name;
+            link.setAttribute("href", curItemData.url);
+            pContainer.appendChild(link);
+
+            let shortcutDisplay = document.createElement("span");
+            shortcutDisplay.innerHTML = curItemData.shortcutKey;
+            shortcutDisplay.className = "shortcut";
+            shortcutDisplay.style.animation = "none";
+            pContainer.appendChild(shortcutDisplay);
+
+            getUrl[curItemData.shortcutKey] = curItemData.url
+        }
+    }
+}
+
+function shortcutListener(e) {
+    let key = e.key.toLowerCase();
+
+    if (listeningForShortcut && getUrl.hasOwnProperty(key)){
+        window.location = getUrl[key];
+    }
+
+    if (key === SHORTCUT_STARTER) {
+        clearTimeout(listenerTimeout);
+        listeningForShortcut = true;
+
+        // Animation reset
+        for (let i = 0; i < $shortcutDisplayList.length; i++){
+            $shortcutDisplayList[i].style.animation = "none";
+            setTimeout(function() { $shortcutDisplayList[i].style.animation = ''; }, 10);
+        }
+
+        listenerTimeout = setTimeout(function(){ listeningForShortcut = false; }, SHORTCUT_TIMEOUT);
+    }
+}
+
+function main(){
+    setupWelcomeMessage();
+    setupGroups();
+    document.addEventListener('keyup', shortcutListener, false);
+}
+
+main();
